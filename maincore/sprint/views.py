9ef2a5e2332b9608607a20fc882d3f790ba9ecde
stas_ <dromanqs@mail.ru>
2024-04-18 21:14:43 +0500

    изменение ответов при создании

--- maincore/sprint/views.py
+++ maincore/sprint/views.py
@@ -34,61 +34,31 @@ class MountViewSet(viewsets.ModelViewSet):
         if self.action == 'create':
             serializer = AddMountSerializer(data=request.data)
 
-            # if not serializer.is_valid():
-            #     data = {'error': 'Что-то пошло не так ...', 'status': 'HTTP_400_BAD_REQUEST'}
-            #     return Response(data, status=status.HTTP_400_BAD_REQUEST)
-            #
-            # if serializer.is_valid():
-            #     serializer.save()
-            #     data = {'state': 0, 'message': 'Изменение невозможно. Информация на проверке модератора или принята'}
-            #
-            #     return Response(data, status=status.HTTP_200_OK)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(
+                    {
+                        'status': status.HTTP_200_OK,
+                        'message': 'Успех!',
+                        'id': serializer.instance.pk,
+                    }
+                )
 
-            # if status.HTTP_400_BAD_REQUEST:
-            #     return Response(
-            #         {
-            #             'status': status.HTTP_400_BAD_REQUEST,
-            #             'message': 'Некорректный запрос',
-            #             'id': None,
-            #         }
-            #     )
-            #
-            # if status.HTTP_500_INTERNAL_SERVER_ERROR:
-            #     return Response(
-            #         {
-            #             'status': status.HTTP_500_INTERNAL_SERVER_ERROR,
-            #             'message': 'Ошибка при выполнении операции',
-            #             'id': None,
-            #         }
-            #     )
+            if status.HTTP_400_BAD_REQUEST:
+                return Response(
+                    {
+                        'status': status.HTTP_400_BAD_REQUEST,
+                        'message': 'Некорректный запрос',
+                        'id': None,
+                    }
+                )
+
+            if status.HTTP_500_INTERNAL_SERVER_ERROR:
+                return Response(
+                    {
+                        'status': status.HTTP_500_INTERNAL_SERVER_ERROR,
+                        'message': 'Ошибка при выполнении операции',
+                        'id': None,
+                    }
+                )
         return super().create(request, *args, **kwargs)
-    #Создаем перевал
-    # def create(self, request, *args, **kwargs):
-    #     serializer = AddMountSerializer(data=request.data)
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return Response(
-    #             {
-    #                 'status': status.HTTP_200_OK,
-    #                 'message': 'Успешно!',
-    #                 'id': serializer.data['id'],
-    #             }
-    #         )
-
-        # if status.HTTP_400_BAD_REQUEST:
-        #     return Response(
-        #         {
-        #             'status': status.HTTP_400_BAD_REQUEST,
-        #             'message': 'Неккоректный запрос',
-        #             'id': None,
-        #         }
-        #     )
-
-        # if status.HTTP_500_INTERNAL_SERVER_ERROR:
-        #     return Response(
-        #         {
-        #             'status': status.HTTP_500_INTERNAL_SERVER_ERROR,
-        #             'message': 'Ошибка сервера',
-        #             'id': None,
-        #         }
-        #     )
